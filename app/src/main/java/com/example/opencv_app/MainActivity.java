cd9b8cca659929e7d8ea0dacd4b5c6492376f1a2
lkjj-lkjj <20301105@bjtu.edu.cn>
2023-10-29 12:44:15 +0800

    face detect

--- app/src/main/java/com/example/opencv_app/MainActivity.java
+++ app/src/main/java/com/example/opencv_app/MainActivity.java
@@ -1,25 +1,183 @@
 package com.example.opencv_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.*;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.tbruyelle.rxpermissions3.RxPermissions;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+/**
+ * 人脸检测应用的主活动
+ */
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat mIntermediateMat;
+    private CascadeClassifier classifier;
+    private Mat mGray;
+    private Mat mRgba;
+    private int mAbsoluteFaceSize = 0;
+
+    // OpenCV加载回调，确保OpenCV已成功加载
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    getPermission(); // 获取相机权限
+                    mOpenCvCameraView.enableView(); // 启用相机视图
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    // 构造函数
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /**
+     * 当活动首次创建时调用
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (OpenCVLoader.initDebug()) {
-            Toast toast = Toast.makeText(getApplicationContext(), "opencv加载成功", Toast.LENGTH_SHORT);
-            toast.show();
-        }else {
-            Toast toast = Toast.makeText(getApplicationContext(), "opencv加载失败", Toast.LENGTH_SHORT);
-            toast.show();
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.by);
+        mOpenCvCameraView.enableView();
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT); // 使用前置摄像头
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView(); // 暂停时禁用相机视图
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    public void onCameraViewStopped() {
+        // 显式释放Mats
+        if (mIntermediateMat != null)
+            mIntermediateMat.release();
+        mIntermediateMat = null;
+        mGray.release();
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        // 判断横竖屏用于进行图像的旋转
+        if(getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT){
+            Core.flip(mRgba, mRgba, 1); // 翻转图像以适应竖屏
+            Core.flip(mGray, mGray, 1); // 翻转灰度图像
+        }
+
+        float mRelativeFaceSize = 0.2f;
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+
+        MatOfRect faces = new MatOfRect();
+        if (classifier != null)
+            classifier.detectMultiScale(mGray, faces, 1.1, 2, 2,
+                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+
+        Rect[] facesArray = faces.toArray();
+        Scalar faceRectColor = new Scalar(0, 255, 0, 255);
+        for (Rect faceRect : facesArray)
+            Imgproc.rectangle(mRgba, faceRect.tl(), faceRect.br(), faceRectColor, 3); // 绘制矩形框
+
+        Log.e(TAG, "共检测到 " + faces.toArray().length + " 张脸");
+        return mRgba;
+    }
+
+    // 初始化人脸级联分类器，必须先初始化
+    private void initClassifier() {
+        try {
+            InputStream is = getResources()
+                    .openRawResource(R.raw.lbpcascade_frontalface);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File cascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+            FileOutputStream os = new FileOutputStream(cascadeFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+            classifier = new CascadeClassifier(cascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private void getPermission(){
+        RxPermissions rxPermissions = new RxPermissions(MainActivity.this);
+        rxPermissions.request(Manifest.permission.CAMERA).subscribe(aBoolean -> {
+            if (aBoolean) {
+                initClassifier();
+                mOpenCvCameraView.setCameraPermissionGranted();
+            } else {
+                Toast.makeText(MainActivity.this, "您还没有开启相机权限,请前往设置->应用管理->开启", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
